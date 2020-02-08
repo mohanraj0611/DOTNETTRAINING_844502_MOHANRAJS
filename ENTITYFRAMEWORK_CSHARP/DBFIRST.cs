8fb5c7682804fb98714f5aa5eb96adc2f6a75def
mohanraj0611 <39555585+mohanraj0611@users.noreply.github.com>
2020-02-08 16:30:05 +0530

    Add files via upload

---
+++ ENTITYFRAMEWORK_CSHARP/DBFIRST.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleDBfirstEDFP
+{
+    class Program
+    {
+      
+       
+
+        static void Main(string[] args)
+        {
+            EmployeeEFDBEntities context = new EmployeeEFDBEntities();
+
+            //var Employee = new Employee()
+            //{
+            //    EmpID = 1,
+            //    Name = "mohan",
+            //    Salary = 28000.00,
+            //    SSN = 23456,
+            //    DepID = 3
+
+            //};
+            //context.Employees.Add(Employee);
+
+            //var Employee2 = new Employee()
+            //{
+            //    EmpID = 2,
+            //    Name = "vimal",
+            //    Salary = 27000.00,
+            //    SSN = 24458,
+            //    DepID = 3
+
+            //};
+            //context.Employees.Add(Employee2);
+
+            //var Employee1 = new Employee()
+            //{
+            //    EmpID = 3,
+            //    Name = "mtuhin",
+            //    Salary = 29000.00,
+            //    SSN = 23457,
+            //    DepID = 3
+
+            //};
+            //context.Employees.Add(Employee1);
+            //context.SaveChanges();
+
+            LinqToEntity();
+            modifyemployee();
+            deleteemployee();
+
+
+
+        }
+        public static void LinqToEntity()
+        {
+            EmployeeEFDBEntities context = new EmployeeEFDBEntities();
+
+            var employees = context.Employees;
+
+            var emplist = from emp in employees
+                          orderby emp.Salary
+                          select emp;
+            foreach(var emp in employees)
+            {
+                Console.WriteLine("EmpId:{0},Name:{1},salary:{2},SSN:{3},DepID:{4}", emp.EmpID, emp.Name, emp.Salary, emp.SSN, emp.DepID);
+            }
+        }
+
+        public static void modifyemployee()
+        {
+            EmployeeEFDBEntities context = new EmployeeEFDBEntities();
+
+            var employees = context.Employees;
+
+            var emplist = from emp in employees
+                         orderby emp.Salary
+                        select emp;
+
+            foreach (var emp in employees)
+            {
+                if(emp.EmpID==1)
+                {
+                    emp.Name = "johny";
+                }
+            }
+            context.SaveChanges();
+        }
+
+        public static void deleteemployee()
+        {
+            EmployeeEFDBEntities context = new EmployeeEFDBEntities();
+
+            var employees = context.Employees;
+
+            var employee = new Employee { EmpID = 5 };
+            employees.Attach(employee);
+            employees.Remove(employee);
+            context.SaveChanges();
+        }
+    }
+}
